c19819168630412461f0501f9fd05c8336f5ded3
abhitech2002 <oabhishek31@gmail.com>
2023-07-29 15:35:31 +0530

    Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './Home.css';
+import Typewriter from 'typewriter-effect'
+// import Resume from '../../assets/docs/Abhishek_Ojha.pdf'
 
 const Home = () => {
     return (
-        <div>
-            <h1>
-                Home Page
-            </h1>
-        </div>
+        <>
+            <div className='container-fluid home-container'>
+                <div className='container home-content'>
+                    <h1>Hi I'm a </h1>
+                    <h2>
+                        <Typewriter
+                            options={{
+                                strings: ['FullStack Developer !', 'MERN Stack Developer !'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </h2>
+                    <div className='home-buttons'>
+                        <button className='btn btn-hire'>Hire Me</button>
+                        <a className='btn btn-cv' href='../../assets/docs/Abhishek_Ojha.pdf' download='Abhishek_Ojha.pdf'>My Resume</a>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
