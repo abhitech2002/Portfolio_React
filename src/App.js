f98b596ed76e57d1de06e7759c2de812152c1fe7
abhitech2002 <oabhishek31@gmail.com>
2023-08-24 22:59:40 +0530

    About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Layout from "./components/Layout/Layout";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <>
       <Layout />
+      <div className="">
+        <About />
+      </div>
+
     </>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Home.css';
 import Typewriter from 'typewriter-effect'
-// import Resume from '../../assets/docs/Abhishek_Ojha.pdf'
+// import Resume from '../../assets/docs/AbhishekOjha.pdf'
 
 const Home = () => {
     return (
         <>
             <div className='container-fluid home-container'>
                 <div className='container home-content'>
-                    <h1>Hi I'm a </h1>
-                    <h2>
+                    <h2>Hi I'm a</h2>
+                    <h1>
                         <Typewriter
                             options={{
                                 strings: ['FullStack Developer !', 'MERN Stack Developer !'],
@@ -17,10 +17,10 @@ const Home = () => {
                                 loop: true,
                             }}
                         />
-                    </h2>
+                    </h1>
                     <div className='home-buttons'>
                         <button className='btn btn-hire'>Hire Me</button>
-                        <a className='btn btn-cv' href='../../assets/docs/Abhishek_Ojha.pdf' download='Abhishek_Ojha.pdf'>My Resume</a>
+                        <a className='btn btn-cv' href='../../assets/docs/AbhishekOjha.pdf' download='AbhishekOjha.pdf'>My Resume</a>
                     </div>
                 </div>
             </div>
